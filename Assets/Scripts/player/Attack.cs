f28026fa1bb12977df68809fbe4bcf2609d7efff
acferM <matheus.acfer@nave.org.br>
2021-08-22 21:45:30 -0300

    morri ?

--- Assets/Scripts/player/Attack.cs
+++ Assets/Scripts/player/Attack.cs
@@ -11,8 +11,10 @@ public class Attack : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        
+        if (other.gameObject.tag == "Enemy")
         {
+            
             other.GetComponent<EnemyHealth>().takeDamage();
         }    
     }
